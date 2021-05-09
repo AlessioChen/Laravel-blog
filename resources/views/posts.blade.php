f18320fe7f035446073135e5afc0a9aa3dc3602e
alessio <alessiochen98@gmail.com>
2021-05-09 21:27:57 +0200

    fix: add tailwindcss cdn for heroku

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
     <title>Laravel blog </title>
 </head>
 
